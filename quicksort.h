4d193cd9eff662829be5c5218a9a05520cb15454
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-06-10 10:37:35 -0700

    Create quicksort.h

---
+++ quicksort.h
@@ -0,0 +1,12 @@
+//
+// Created by Colby on 6/9/2022.
+//
+
+#ifndef SORTTEST_QUICKSORT_H
+#define SORTTEST_QUICKSORT_H
+
+void swap(int *a, int *b);
+int  partition(int array[], int low, int high);
+void quickSort(int array[], int low, int high);
+
+#endif //SORTTEST_QUICKSORT_H
