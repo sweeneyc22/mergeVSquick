0253affdef48a9e77796fe3347fcfc5c65c0d222
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-06-10 10:36:51 -0700

    Create mergesort.h

---
+++ mergesort.h
@@ -0,0 +1,12 @@
+//
+// Created by Colby on 6/10/2022.
+//
+
+#ifndef SORTTEST_MERGESORT_H
+#define SORTTEST_MERGESORT_H
+
+void merge(int arr[], int p, int q, int r);
+void mergeSort(int arr[], int l, int r);
+
+
+#endif //SORTTEST_MERGESORT_H
